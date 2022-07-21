714dacca98bf434ecc85b02b5069e2ac9c7b7d41
AriesRedWar <philipjbowman5@gmail.com>
2022-07-20 23:14:56 -0400

    added admin site ability to change the QTY of the book available amount

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
 
-// Your Code Here
+// // Your Code Here
+    async function main() {
+        let response = await fetch('http://localhost:3001/listBooks')
+        let books = await response.json()
+        books.forEach(listBook)
+    }
+    
+    function listBook(book) {
+        let root = document.querySelector('#root')
+        // List of the books on the site
+        let li = document.createElement('li')
+        li.textContent = book.title + " = "
+        // input field for the QTY of books availible
+        let quantityInput = document.createElement('input')
+        quantityInput.value = book.quantity
+        // Admins save button to update the Book qty availible
+        let saveButton = document.createElement('button')
+        saveButton.textContent = 'Save'
+        // Event Listener to actualy update the books 
+        saveButton.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: quantityInput.value
+                })
+            })
+        })    
+        li.append(quantityInput, saveButton)    
+        root.append(li)
+    }
+    
+    main();
